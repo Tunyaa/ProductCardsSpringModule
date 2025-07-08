88cd2841caaa86ad54161e84386cd498e63c518e
tunyaa <ildcampthunder@gmail.com>
2025-07-09 01:38:21 +0300

    init

---
+++ src/main/java/module/ProductCardsSpringModule/ProductCardsSpringModuleApplication.java
@@ -0,0 +1,13 @@
+package module.ProductCardsSpringModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCardsSpringModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCardsSpringModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/module/ProductCardsSpringModule/ProductCardsSpringModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package module.ProductCardsSpringModule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCardsSpringModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
