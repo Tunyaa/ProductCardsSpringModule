3c381e73a26138f59fee63bcee163d829bc45b45
tunyaa <ildcampthunder@gmail.com>
2025-08-25 18:24:14 +0300

    OK: +html/css +Controller

--- src/main/java/module/ProductCardsSpringModule/controller/ExecuteOrderController.java
+++ src/main/java/module/ProductCardsSpringModule/controller/ExecuteOrderController.java
@@ -1,9 +1,13 @@
 package module.ProductCardsSpringModule.controller;
 
+import java.util.List;
+import module.ProductCardsSpringModule.model.Order;
+import module.ProductCardsSpringModule.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -13,10 +17,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/execute")
 public class ExecuteOrderController {
 
+    private final OrderService orderService;
+
+    public ExecuteOrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @GetMapping("/execute_order")
     public String executeOrderByOrderId(
-            Model model
+            Model model,
+            @RequestParam Long id
     ) {
+        System.out.println(id + " - ID");
+//        orderService.getPositionsByOrderId(id);
+        
+        List<Order> orders = orderService.getAllOrders();
+        model.addAttribute("orders", orders);
         return "execute_order";
     }
 }
